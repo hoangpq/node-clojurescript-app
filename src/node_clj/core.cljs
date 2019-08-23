b95da16f19d8db0b299d2d106d83efa0a9096e8c
Hoang Phan <hoangpq@users.noreply.github.com>
2019-08-23 10:41:58 +0700

    Format clj

--- src/node_clj/core.cljs
+++ src/node_clj/core.cljs
@@ -2,43 +2,36 @@
   (:require [cljs.nodejs :as nodejs]
             [clojure.string :as string]))
 
-  (nodejs/enable-util-print!)
+(nodejs/enable-util-print!)
 
-  (defonce express (nodejs/require "express"))
-  (defonce http (nodejs/require "http"))
-  (defonce server-port 3000)
+(defonce express (nodejs/require "express"))
+(defonce http (nodejs/require "http"))
+(defonce server-port 3000)
 
-  ;; app gets redefined on reload
-  (def app (express))
+;; app gets redefined on reload
+(def app (express))
 
-  ;; req.params[key]
-  (defn req-params [req key]
-    (aget req "params" key))
+;; req.params[key]
+(defn req-params [req key]
+  (aget req "params" key))
 
-  (defn factorial [n]
-    (loop [current n fact 1]
-      (if (= current 1)
+(defn factorial [n]
+  (loop [current n fact 1]
+    (if (= current 1)
       fact
       (recur (dec current) (* fact current)))))
 
-  (. app (get "/hello"
-    (fn [req res] (. res (send "Hello world")))))
+(. app (get "/hello"
+            (fn [_ res] (. res (send "Hello world")))))
 
-  ;; /user/hoangpq
-  (. app (get "/user/:name"
-    (fn [req res] (. res (send (req-params req "name"))))))
+(. app (get "/user/:name"
+            (fn [req res] (. res (send (req-params req "name"))))))
 
-  ;; /fact/10
-  (. app (get "/fact/:number"
-    (fn [req res]
-    (let [number (int (req-params req "number"))]
-      (. res (send (string/join ["Factorial(" number ") = " (factorial number)])))))))
+(. app (get "/fact/:number"
+            (fn [req res]
+              (let [number (int (req-params req "number"))]
+                (. res (send (string/join ["Factorial(" number ") = " (factorial number)])))))))
+(def -main
+  (.listen app server-port (fn [] (println "Server running on" server-port))))
 
-  (def -main
-    (fn []
-      (doto (.createServer http #(app %1 %2))
-        (.listen server-port))))
-      (println (string/join " " ["Server running on" server-port]) )
-
-
-  (set! *main-cli-fn* -main)
+(set! *main-cli-fn* -main)
